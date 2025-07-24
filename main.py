c45c94c09cf338c3b98778877982173fa0ede3bb
Roxann <rofaniry@gmail.com>
2025-07-24 09:34:29 +0300

    reponses des questions

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
 from fastapi import FastAPI
+import json
+from fastapi import FastAPI, Request
+from starlette.responses import Response, JSONResponse
+
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get("/")
+@app.get("/hello")
 async def read_root():
-    return {"message": "Hello sur API FastAPI !"}
+    return JSONResponse (content={"message": "Hello world"}, status_code=200)
+
+@app.get("/welcome")
+def welcome_user(name : str):
+    return JSONResponse({"message": f"Welcome {name}"}, status_code=200)
+
+
+class StudentModel(BaseModel):
+    reference: str
+    firstName: str
+    lastName: str
+    age: int
+
+students_store: List[StudentModel] = []
+
+def serialized_stored_students():
+    return [student.model_dump() for student in students_store]
+
+@app.post("/students")
+def add_students(new_student: List[StudentModel]):
+    students_store.extend(new_student)
+    return {"students": serialized_stored_students()}
+
+@app.get("/students")
+def list_students():
+    return JSONResponse(content={"students": serialized_stored_students()}, status_code=200)
+
+@app.put("/students")
+def reload_students(new_student: List[StudentModel]):
+    students_store.clear()
+    students_store.extend(new_student)
+    return  {"students": serialized_stored_students()}
